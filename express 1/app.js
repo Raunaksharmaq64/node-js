c1082528399dff1f50e34227feef72450fb4d751
raunaksharmaq64 <raunaksharma88630mt@gmail.com>
2026-02-15 11:02:26 +0530

    adding practice set

---
+++ express 1/app.js
@@ -0,0 +1,11 @@
+//////  basic code for starting server by node after addind all the dependency of nodemon or express
+const express=require('express');
+
+const app=express();
+
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
